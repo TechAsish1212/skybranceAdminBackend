186bf340f7fcac4adb8a8f2316b4bac2855d7651
TechAsish1212 <gitasish1212@gmail.com>
2026-05-22 22:04:59 +0530

    feat: add verify otp routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signin, signup } from "../controllers/auth.controller";
+import { signin, signup, verifyEmail } from "../controllers/auth.controller";
 
 const authRoutes=Router();
 
 authRoutes.post('/signup',signup);
+authRoutes.post('/verify-otp',verifyEmail);
 authRoutes.post('/signin',signin);
 
 
