0302a93ba8c60ce9bf0cd0506114b05a5552cca9
TechAsish1212 <gitasish1212@gmail.com>
2026-05-22 23:38:26 +0530

    feat: add resend otp route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signin, signup, verifyEmail } from "../controllers/auth.controller";
+import { resendOTP, signin, signup, verifyEmail } from "../controllers/auth.controller";
 
 const authRoutes=Router();
 
 authRoutes.post('/signup',signup);
 authRoutes.post('/verify-otp',verifyEmail);
+authRoutes.post('/resend-otp',resendOTP);
 authRoutes.post('/signin',signin);
 
 
