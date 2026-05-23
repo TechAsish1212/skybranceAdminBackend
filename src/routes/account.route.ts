b1d8b763176f71fe4ed369174f0786ff7de0ea1a
TechAsish1212 <gitasish1212@gmail.com>
2026-05-24 00:30:14 +0530

    feat: add change password route

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth";
-import { getProfile, updateProfile } from "../controllers/account.controller";
+import { changePassword, getProfile, updateProfile } from "../controllers/account.controller";
 
 const accountRoutes = Router();
 
 accountRoutes.get('/', authenticate, getProfile);
 accountRoutes.patch('/update-profile', authenticate, updateProfile);
+accountRoutes.post('/change-password',authenticate,changePassword);
 
 export default accountRoutes;
