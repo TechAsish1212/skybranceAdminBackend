4e81e8f28a2f39ac9ea24f97d1c7f96fafa20ba4
TechAsish1212 <gitasish1212@gmail.com>
2026-05-23 22:17:36 +0530

    feat: add forgot password ,reset password route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
-import { resendOTP, signin, signout, signup, verifyEmail } from "../controllers/auth.controller";
+import { forgotPassword, resendOTP, resetPassword, signin, signout, signup, verifyEmail } from "../controllers/auth.controller";
 import { authenticate } from "../middlewares/auth";
 
-const authRoutes=Router();
+const authRoutes = Router();
 
-authRoutes.post('/signup',signup);
-authRoutes.post('/verify-otp',verifyEmail);
-authRoutes.post('/resend-otp',resendOTP);
-authRoutes.post('/signin',signin);
-authRoutes.post('/signout',signout);
+authRoutes.post('/signup', signup);
+authRoutes.post('/verify-otp', verifyEmail);
+authRoutes.post('/resend-otp', resendOTP);
+authRoutes.post('/signin', signin);
+authRoutes.post('/signout', signout);
+
+authRoutes.post('/forgot-password', forgotPassword);
+authRoutes.post('/reset-password', resetPassword)
 
 
 export default authRoutes;
