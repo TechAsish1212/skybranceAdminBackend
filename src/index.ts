4a0326bf099e31c3c8106076ec2e14f957aa4e21
TechAsish1212 <gitasish1212@gmail.com>
2026-05-23 22:29:28 +0530

    feat:: add middleware and endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import { config } from 'dotenv';
 import connectDB from './db/db';
+import routes from './routes';
 
 
 
@@ -12,11 +13,18 @@ config();
 // connect database
 connectDB();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ success: true, message: 'API is running.......' });
 })
+
+// endpoint
+app.use('/api',routes);
 
 
 app.listen(PORT, () => {
