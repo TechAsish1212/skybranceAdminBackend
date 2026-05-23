6985d880ae5e80f68c6b7794e1089761909605c7
TechAsish1212 <gitasish1212@gmail.com>
2026-05-24 00:31:31 +0530

    feat: add account routes in root router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.route";
+import accountRoutes from "./account.route";
 
 const routes=Router();
 
 routes.use('/auth',authRoutes);
+routes.use('/account',accountRoutes);
 
 export default routes;
